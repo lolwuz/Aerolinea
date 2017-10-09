99a8a3bd39c6d7c5a42bce105f303a11d2d9cf18
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-09 12:08:45 +0200

    Orbit
    fix

--- Aerolínea/objects/World.js
+++ Aerolínea/objects/World.js
@@ -2,8 +2,10 @@ class World extends THREE.Object3D{
     constructor() {
         super();
      
-        let earthGeometry = new THREE.SphereGeometry(50, 128, 128);
+        let earthGeometry = new THREE.SphereGeometry(1, 128, 128);
+        let earthTexture = new THREE.TextureLoader().load( "./src/textures/earth8k.jpg" );
         let earthMaterial = new THREE.MeshStandardMaterial({
+            map: earthTexture,
             color: 0xFFFFFF
         });
         let earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
