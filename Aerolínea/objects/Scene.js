845dca1a0c180cd2eb279dc114d3148a0319092a
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-09 11:45:05 +0200

    World

--- Aerolínea/objects/Scene.js
+++ Aerolínea/objects/Scene.js
@@ -7,20 +7,30 @@ class Scene extends THREE.Scene {
             antialias: true
         });
         this.renderer.setClearColor(0x4f99ff, 1);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
-
+        
         // Add orbit controlls to the scene.
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-        this.controls.maxDistance = 200;
-        this.controls.minDistance = 5;
+        this.controls.maxDistance = 100;
+        this.controls.minDistance = 50;
         this.controls.enablePan = false;
         this.controls.enableRotate = true;
         this.controls.update();
-
-        this.add();
+        
+        this.world = new World(); 
+        this.add(this.world);
+        
+        this.addLight();
+    }
+    
+    addLight(){
+        let ambient = new THREE.AmbientLight(0xFFFFFF);
+        this.add(ambient);
     }
     
     update(){
         this.renderer.render(this, this.camera);
+        this.world.update();
     }
 }
--- Aerolínea/objects/World.js
+++ Aerolínea/objects/World.js
@@ -1,12 +1,16 @@
 class World extends THREE.Object3D{
     constructor() {
         super();
-        
-        let earthGeometry = new THREE.SphereGeometry(6, 32, 32);
+     
+        let earthGeometry = new THREE.SphereGeometry(50, 128, 128);
         let earthMaterial = new THREE.MeshStandardMaterial({
             color: 0xFFFFFF
         });
         let earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
         this.add(earthMesh);
+    }
+    
+    update(){
+        
     }
 }
