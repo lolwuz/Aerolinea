20be262c52488d526169fc70e49a5139eb5ce428
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-09 18:01:18 +0200

    Pimp my earth

--- Aerolínea/objects/World.js
+++ Aerolínea/objects/World.js
@@ -1,18 +1,27 @@
-class World extends THREE.Object3D{
+class World extends THREE.Object3D {
     constructor() {
         super();
-     
-        let earthGeometry = new THREE.SphereGeometry(1, 128, 128);
-        let earthTexture = new THREE.TextureLoader().load( "./src/textures/earth8k.jpg" );
+
+        let earthGeometry = new THREE.SphereGeometry(1, 64, 64);
+        let earthTexture = new THREE.TextureLoader().load("./src/textures/Earth.jpg");
+        let earthBump = new THREE.TextureLoader().load("./src/textures/EarthBump.jpg");
+        let earthRough = new THREE.TextureLoader().load("./src/textures/EarthSpec.jpg");
+
         let earthMaterial = new THREE.MeshStandardMaterial({
+            metalness: 0,
+            roughness: 0.4,
             map: earthTexture,
+            bumpMap: earthBump,
+            roughnessMap: earthRough,
+            bumpScale: 0.005,
             color: 0xFFFFFF
         });
+
         let earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
         this.add(earthMesh);
     }
-    
-    update(){
-        
+
+    update() {
+
     }
 }
