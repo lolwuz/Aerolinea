1993d59d9cfac5e4bd2555d4294a2f3433e07f16
Koen <koenzk@hotmail.com>
2017-10-10 14:53:36 +0200

    Added scene to canvas

--- Aerolínea/objects/Scene.js
+++ Aerolínea/objects/Scene.js
@@ -3,13 +3,16 @@ class Scene extends THREE.Scene {
         super();
         // Add camera to the scene.
         this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.01, 10);
-        this.renderer = new THREE.WebGLRenderer({
-            antialias: true
-        });
+        this.renderer = new THREE.WebGLRenderer(
+            {
+                antialias: true,
+                canvas: gameCanvas
+            }
+        );
         this.renderer.setClearColor(0x4f99ff, 1);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
-        
+
         // Add orbit controlls to the scene.
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
         this.controls.maxDistance = 10;
@@ -17,22 +20,24 @@ class Scene extends THREE.Scene {
         this.controls.enablePan = false;
         this.controls.enableRotate = true;
         this.controls.update();
-        
-        this.world = new World(); 
+
+        this.world = new World();
         this.add(this.world);
-        
+
+        // this.add(new Plane());
+
         this.addLight();
     }
-    
-    addLight(){
+
+    addLight() {
         let ambient = new THREE.AmbientLight(0xFFFFFF, 0.2);
         let sun = new THREE.DirectionalLight(0xFFFFFF, 0.6);
 
         this.add(ambient);
-        this.add(sun); 
+        this.add(sun);
     }
-    
-    update(){
+
+    update() {
         this.renderer.render(this, this.camera);
         this.world.update();
     }
