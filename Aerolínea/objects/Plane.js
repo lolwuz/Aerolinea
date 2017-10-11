ecfb3bc4f1bdd74ec700207d9e4c4e68c009f5cf
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-11 11:55:59 +0200

    Plane

--- Aerolínea/objects/Plane.js
+++ Aerolínea/objects/Plane.js
@@ -2,17 +2,23 @@ class Plane extends THREE.Object3D {
     constructor() {
         super();
 
+        // TODO: Build plane(group/Object3D), Textures, Specific functions etc...
+        // Details: Sizes, position, rotation of all parts of the Object3D/group.
+
         let body = Plane.body();
         let nose = Plane.nose();
         let wingRight = Plane.wing(1);
-        let wingLeft = Plane.wing(-1);
-        let tail = Plane.tail();
+        // let wingLeft = Plane.wing(-1);
+        // let tail = Plane.tail();
 
         this.add(body);
         this.add(nose);
         this.add(wingRight);
-        this.add(wingLeft);
-        this.add(tail);
+        // this.add(wingLeft);
+        // this.add(tail);
+
+        // this.position.set();
+        // this.rotation.set();
     }
 
     // Move the plane
@@ -20,51 +26,72 @@ class Plane extends THREE.Object3D {
     }
 
     // Method for creating the body/middle of the plane
-    private static body() {
-        let geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
-        let material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    // CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded, thetaStart, thetaLength)
+    static body() {
+        let geometry = new THREE.CylinderGeometry(0.2, 0.2, 0.4, 32);
+        let material = new THREE.MeshBasicMaterial(
+            {
+                color: 0xffffff
+            }
+        );
         let body = new THREE.Mesh(geometry, material);
 
-        body.position.set();
-        body.rotation.set();
+        body.position.set(1, 1, 1);
+        // body.rotation.set();
 
         return body;
     }
 
     // Method for creating the nose of the plane
-    private static nose() {
-        let geometry = new THREE.ConeGeometry(5, 5, 20, 32);
-        let material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    // ConeGeometry(radius, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength)
+    static nose() {
+        let geometry = new THREE.ConeGeometry(0.2, 0.3, 32);
+        let material = new THREE.MeshBasicMaterial(
+            {
+                color: 0xffffff
+            }
+        );
         let nose = new THREE.Mesh(geometry, material);
 
-        nose.position.set();
-        nose.rotation.set();
+        nose.position.set(1, 1.35, 1);
+        // nose.rotation.set();
 
         return nose;
     }
 
     // Method for creating a wing of the plane
-    private static wing(multiplier) {
+    static wing(multiplier) {
         let geometry = new THREE.Geometry();
-        let material = new THREE.MeshBasicMaterial({color: 0xffff00});
+        let material = new THREE.MeshBasicMaterial(
+            {
+                color: 0xffff00
+            }
+        );
+
+        let v1 = new THREE.Vector3(0, 0, 0);
+        let v2 = new THREE.Vector3(1, 0, 0);
+        let v3 = new THREE.Vector3(1, 1, 0);
+
+        geometry.vertices.push(v1);
+        geometry.vertices.push(v2);
+        geometry.vertices.push(v3);
+
+        geometry.faces.push(new THREE.Face3(0, 1, 2));
+        geometry.computeFaceNormals();
+
         let wing = new THREE.Mesh(geometry, material);
 
-        wing.position.set();
-        wing.rotation.set();
+        wing.position.set(1, 1, 1);
+        // wing.rotation.set(new THREE.Vector3( 0, 0, Math.PI / 2));
 
         return wing;
-
     }
 
     // Method for creating the tail of the plane
-    private static tail() {
-        let geometry = new THREE.Geometry();
-        let material = new THREE.MeshBasicMaterial({color: 0xffff0});
-        let tail = new THREE.Mesh(geometry, material);
+    static tail() {
+    }
 
-        tail.position.set();
-        tail.rotation.set();
-
-        return tail;
+    // Method for creating the wing on the tail of the plane
+    static tailWing() {
     }
 }
