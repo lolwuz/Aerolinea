7199a6a4f2a78f9286f4899d9063a857d92f6982
MichLad <lade1400@student.nhl.nl>
2017-10-17 12:23:49 +0200

    Airplane obj imports correctly
    And a lot of excess files are deleted to clean things up.

--- objects/Airplane.js
+++ objects/Airplane.js
@@ -3,56 +3,30 @@ class Airplane extends THREE.Object3D {
         super();
 
         let tempClass = this;
-
-        let mtlLoader = new THREE.MTLLoader();
-        mtlLoader.setPath('src/');
-        mtlLoader.load('Airbus A310.mtl', function(materials) {
-            materials.preload();
-
-            console.log(materials);
-
-            let objLoader = new THREE.OBJLoader();
-            objLoader.setMaterials(materials);
-            objLoader.setPath('src/');
-            objLoader.load('Airbus A310.obj', function(object) {
-
-                object.scale.set(0.05, 0.05, 0.05);
+        
+        let texture = new THREE.Texture();
+        
+        let imageloader = new THREE.ImageLoader();
+        imageloader.load('models/Boeing787-8/Boeing 787-8 texture.png', function(image) {
+            texture.image = image;
+            texture.needsUpdate = true;
+        });
+        
+        let objLoader = new THREE.OBJLoader();
+            objLoader.setPath('models/Boeing787-8/');
+            objLoader.load('Boeing 787-8.obj', function(object) {
+                object.traverse(function(child) {
+                    if (child instanceof THREE.Mesh) {
+                        child.material.map = texture;
+                    }
+                })
+                
+                object.scale.set(1, 1, 1);
                 object.position.set(1.5, 1.5, 1.5);
 
                 tempClass.add(object);
+                console.log(tempClass);
             });
-
-        });
-
-        // OBJLoader:
-        // let loader = new THREE.OBJLoader();
-        // loader.load('src/Airbus A310.obj', function (object) {
-        //     object.traverse(function (child) {
-        //         if (child instanceof THREE.Mesh) {
-        //             child.material.map = texture;
-        //         }
-        //     });
-        //
-        //     object.scale.set(0.05, 0.05, 0.05);
-        //     object.position.set(1.5, 1.5, 1.5);
-        //
-        //     // console.log(object);
-        //
-        //     tempClass.add(object);
-        // });
-
-        // ColladaLoader:
-        // let loader = new THREE.ColladaLoader();
-        // loader.load("src/galaxy.dae", function (result) {
-        //     let boeing = result.scene;
-        //
-        //     boeing.scale.set(0.0001, 0.0001, 0.0001);
-        //     boeing.position.set(1, 1, 1);
-        //
-        //     // console.log(boeing);
-        //
-        //     tempClass.add(boeing);
-        // });
     }
 
     applyRotation(stepZ, stepY) {
