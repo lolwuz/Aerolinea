e5b12bd423f00f3920614847a9d8af23ec6a958c
Marten Hoekstra <martenhoekstra2@gmail.com>
2017-10-20 17:29:18 +0200

    Add Airports and Routes by way of API

--- objects/Scene.js
+++ objects/Scene.js
@@ -31,36 +31,37 @@ class Scene extends THREE.Scene {
         this.addLight();
         this.add(new axisHelper(5));
         
-            this.airplane = new Airplane(0.05, 3, 0.1);
-//            this.add(this.airplane);
+        this.airplane = new Airplane(0.05, 3, 0.1);
 
-            let Geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-            let Material = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
-        
-            this.airport1 = new THREE.Mesh(Geometry, Material);
-            this.airport1.position.y = Math.sin(10*Math.PI/180);
-            let hypXZ = Math.cos(10*Math.PI/180);
-            this.airport1.position.z = Math.sin(-22*Math.PI/180) * hypXZ;
-            this.airport1.position.x = Math.cos(22*Math.PI/180) * hypXZ;
 
-            this.airport2 = new THREE.Mesh(Geometry, Material);
-            this.airport2.position.y = Math.sin(-30*Math.PI/180);
-            hypXZ = Math.cos(-30*Math.PI/180);
-            this.airport2.position.z = Math.sin(-130*Math.PI/180) * hypXZ;
-            this.airport2.position.x = Math.cos(130*Math.PI/180) * hypXZ;
-        
-            this.airport3 = new THREE.Mesh(Geometry, Material);
-            this.airport3.position.y = Math.sin(40*Math.PI/180);
-            hypXZ = Math.cos(-40*Math.PI/180);
-            this.airport3.position.z = Math.sin(-160*Math.PI/180) * hypXZ;
-            this.airport3.position.x = Math.cos(160*Math.PI/180) * hypXZ;
+        let Geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+        let Material = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
+    
+        this.airport1 = new THREE.Mesh(Geometry, Material);
+        this.airport1.position.y = Math.sin(10*Math.PI/180);
+        let hypXZ = Math.cos(10*Math.PI/180);
+        this.airport1.position.z = Math.sin(-22*Math.PI/180) * hypXZ;
+        this.airport1.position.x = Math.cos(22*Math.PI/180) * hypXZ;
+
+        this.airport2 = new THREE.Mesh(Geometry, Material);
+        this.airport2.position.y = Math.sin(-30*Math.PI/180);
+        hypXZ = Math.cos(-30*Math.PI/180);
+        this.airport2.position.z = Math.sin(-130*Math.PI/180) * hypXZ;
+        this.airport2.position.x = Math.cos(130*Math.PI/180) * hypXZ;
+    
+        this.airport3 = new THREE.Mesh(Geometry, Material);
+        this.airport3.position.y = Math.sin(40*Math.PI/180);
+        hypXZ = Math.cos(-40*Math.PI/180);
+        this.airport3.position.z = Math.sin(-160*Math.PI/180) * hypXZ;
+        this.airport3.position.x = Math.cos(160*Math.PI/180) * hypXZ;
         
         this.route = new Route([this.airport1, this.airport2, this.airport3], this.airplane);
         this.add(this.route);
-        console.log(this.route);
-        
-//        this.routeLine = new RouteLine(this.airport1, this.airport2, 0xFFFF00);
-//        this.add(this.routeLine);
+
+        // Fetch
+        this.airportArray = [];
+        this.getAirportsFromAPI(); 
+        this.getRoutesFromAPI();
     }
 
     addLight() {
@@ -76,6 +77,8 @@ class Scene extends THREE.Scene {
         this.add(this.sunPivot);
     }
 
+    
+
     update() {
         // Update lighting
         this.sunPivot.rotation.y += 0.001;
@@ -84,5 +87,36 @@ class Scene extends THREE.Scene {
 
         this.renderer.render(this, this.camera);
 
+    }
+
+    async getAirportsFromAPI(){
+        const response = await fetch("http://server.lolwuz.com:3000/airport");
+        const responseJson = await response.json();
+
+        for(let i = 0; i < responseJson.length; i++){
+            let new_airport = new Airport(responseJson[i]);
+            this.add(new_airport);
+            this.airportArray.push(new_airport);
+        }
+        this.getRoutesFromAPI();
+    }
+
+    async getRoutesFromAPI(){
+        const id = "59e9f093a8c44e1774b155a5";
+        const response = await fetch("http://server.lolwuz.com:3000/route/" + id);
+        const responseJson = await response.json();
+
+        for(let i = 0; i < responseJson.length; i++){
+            let airports = [];
+            for(let ii = 0; ii < responseJson[i].destinations.length; ii++){
+                for(let iii = 0; iii < this.airportArray.length; iii++){
+                    console.log(this.airportArray[iii]._id);
+                    if(responseJson[i].destinations[ii] === this.airportArray[iii].info._id){
+                        airports.push(this.airportArray[iii]);
+                    }
+                }
+            }
+            this.add(new Route(airports)); 
+        }
     }
 }
